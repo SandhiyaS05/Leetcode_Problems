b4300d594cdcebfcc58002405610b07170ead50d
SandhiyaS05 <sandhiyas.cse2023@dce.edu.in>
2026-06-30 21:27:47 +0530

    Time: 12 ms (70.29%), Space: 46.5 MB (61.12%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.java
+++ 0929-unique-email-addresses/0929-unique-email-addresses.java
@@ -1,16 +1,38 @@
 class Solution {
     public int numUniqueEmails(String[] emails) {
-        HashSet<String> set=new HashSet<>();
-        for(String email:emails){
-            String[] parts=email.split("@");
-            String local=parts[0];
-            String domain=parts[1];
-            if(local.contains("+")){
-                local=local.substring(0,local.indexOf("+"));
+       HashSet<String> set = new HashSet<>();
+
+        for (String email : emails) {
+
+            StringBuilder sb = new StringBuilder();
+            int i = 0;
+
+            // Process local name
+            while (email.charAt(i) != '@') {
+
+                if (email.charAt(i) == '+') {
+                    while (email.charAt(i) != '@') {
+                        i++;
+                    }
+                    break;
+                }
+
+                if (email.charAt(i) != '.') {
+                    sb.append(email.charAt(i));
+                }
+
+                i++;
             }
-            local=local.replace(".","");
-            set.add(local + "@" + domain);
+
+            // Add domain name
+            while (i < email.length()) {
+                sb.append(email.charAt(i));
+                i++;
+            }
+
+            set.add(sb.toString());
         }
+
         return set.size();
     }
 }
