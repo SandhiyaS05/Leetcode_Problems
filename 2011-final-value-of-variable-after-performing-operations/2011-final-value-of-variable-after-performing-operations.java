776bdda9f0f72b1563b53c7cb407301f37f9bc2b
SandhiyaS05 <sandhiyas.cse2023@dce.edu.in>
2026-07-02 23:10:31 +0530

    Time: 0 ms (100%), Space: 44.5 MB (29.63%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x=0;
+        for(String it: operations){
+            if(it.charAt(1)=='+'){
+                x++;
+            }
+            else{
+                x--;
+            }
+        }
+        return x;
+    }
+}
