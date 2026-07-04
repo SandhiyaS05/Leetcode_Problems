acb3ca5150bfc224e93da133981583ae7b8c12bf
SandhiyaS05 <sandhiyas.cse2023@dce.edu.in>
2026-07-04 09:45:26 +0530

    Time: 0 ms (100%), Space: 48.2 MB (50.34%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,19 +1,53 @@
 class Solution {
+    int FindFirstIndex(int [] arr,int target){
+        int low =0,high=arr.length-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==target){
+                if(mid==0||arr[mid]!=arr[mid-1]){
+                    return mid;
+                }
+                else{
+                    high=mid-1;
+                }
+            }
+            else if(arr[mid]<target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+       }
+       return -1;
+
+    }
+    int FindLasttIndex(int [] arr,int target){
+        int low =0,high=arr.length-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==target){
+                if(mid==arr.length-1||arr[mid]!=arr[mid+1]){
+                    return mid;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            else if(arr[mid]<target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+       }
+       return -1;
+
+    }
     public int[] searchRange(int[] nums, int target) {
         int [] pos=new int[2];
-        int start=-1,end=-1;       
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                start=i;
-                break;
-            }
-        }
-        for(int i=nums.length-1;i>=0;i--){
-            if(nums[i]==target){
-                end=i;
-                break;
-            }
-        }
+        int start,end;       
+        start=FindFirstIndex(nums,target);
+        end=FindLasttIndex(nums,target);
         pos[0]=start;
         pos[1]=end;
         return pos;
